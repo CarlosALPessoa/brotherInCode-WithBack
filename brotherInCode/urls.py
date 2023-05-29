68c2c3d9bf2eb3c28e6e4690026efe47831813e7
Carlos ALeandro <80118142+CarlosALPessoa@users.noreply.github.com>
2023-05-28 22:49:26 -0300

    Add files via upload

---
+++ brotherInCode/urls.py
@@ -0,0 +1,26 @@
+from rest_framework import routers
+
+from .views import *
+
+#By Carlos
+from django.contrib import admin
+from django.urls import path
+from brotherInCode.views import home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', home)
+]
+#fim by Carlos
+
+routerBrotherInCode = routers.DefaultRouter()
+routerBrotherInCode.register(r'contas', ContasViewSet)
+routerBrotherInCode.register(r'tutores', TutoresViewSet)
+routerBrotherInCode.register(r'alunos', AlunosViewSet)
+routerBrotherInCode.register(r'areaconhecimento', AreaConhecimentoViewSet)
+routerBrotherInCode.register(r'subareasconhecimento', SubareasConhecimentoViewSet)
+routerBrotherInCode.register(r'especializacaotutor', EspecializacaoTutorViewSet)
+routerBrotherInCode.register(r'interessealuno', InteresseAlunoViewSet)
+routerBrotherInCode.register(r'horariostutor', HorariosTutorViewSet)
+routerBrotherInCode.register(r'avaliacaotutor', AvaliacaoTutorViewSet)
+routerBrotherInCode.register(r'tutoria', TutoriaViewSet)
